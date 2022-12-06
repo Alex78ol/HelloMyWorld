5d5d94ad6b27105354adba96567dde9732a00b89
Alex78ol <119539530+Alex78ol@users.noreply.github.com>
2022-12-06 13:15:13 +0100

    Push button set up

--- HelloMyWorld/ViewController.swift
+++ HelloMyWorld/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var greetingBN: UIButton!
+    @IBOutlet var greetingButton: UIButton!
+    @IBOutlet var greetingLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
        
     }
 
-
+    @IBAction func greetingButtonDidTapped() {
+    }
 }
+    
+
+
 
